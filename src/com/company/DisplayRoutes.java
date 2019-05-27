3e1238f62884d418d8906fb16b02bc5dd1a764d7
Jayashree <jayashree.kammu@gmail.com>
2019-05-27 12:15:03 +0530

    get routes from the user

--- src/com/company/DisplayRoutes.java
+++ src/com/company/DisplayRoutes.java
@@ -3,10 +3,11 @@ package com.company;
 import java.util.ArrayList;
 
 public class DisplayRoutes {
-    void listOfRoutes(ArrayList<String> Routes){
-        for(String routes :Routes){
-            System.out.print(" -> "+routes);
+    void listOfRoutes(ArrayList<String> Routes) {
+        for (String routes : Routes) {
+            System.out.print(" -> " + routes);
         }
 
     }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,27 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-            DisplayRoutes Displayroutes = new DisplayRoutes();
-            ArrayList<String> Routes = new ArrayList<>();
-            Routes.add("Tambaram");
-            Routes.add("Sanatorium");
-            Routes.add("Chrompet");
-            Routes.add("Tirusulam");
-            Routes.add("Meenambakkam");
-            Displayroutes.listOfRoutes(Routes);
-        }
+        Scanner keyBoard = new Scanner(System.in);
+        DisplayRoutes Displayroutes = new DisplayRoutes();
+        ArrayList<String> Routes = new ArrayList<>();
+        Routes.add("Tambaram");
+        Routes.add("Sanatorium");
+        Routes.add("Chrompet");
+        Routes.add("Tirusulam");
+        Routes.add("Meenambakkam");
+        Displayroutes.listOfRoutes(Routes);
+        System.out.println();
+        System.out.print("enter you Source :");
+        String source = keyBoard.nextLine();
+        System.out.print("enter you destination :");
+        String destination = keyBoard.nextLine();
+
+
     }
+}
 
