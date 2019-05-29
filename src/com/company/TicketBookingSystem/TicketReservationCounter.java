7e8c7fbf6e28447cbcd345c352ca3d957f0ae134
Jayashree <jayashree.kammu@gmail.com>
2019-05-29 17:52:08 +0530

    get userInput Source and destination

--- src/com/company/TicketBookingSystem/TicketReservationCounter.java
+++ src/com/company/TicketBookingSystem/TicketReservationCounter.java
@@ -1,11 +1,16 @@
 package com.company.TicketBookingSystem;
 
+import java.util.Scanner;
+
 public class TicketReservationCounter {
     public static void main(String[] args) {
-        Routes route=new Routes();
+        Scanner keyboard = new Scanner(System.in);
+        Routes route = new Routes();
         route.routeList();
+        System.out.println();
+        System.out.print("enter you Source :");
+        String source = keyboard.nextLine();
+        System.out.print("enter you destination :");
+        String destination = keyboard.nextLine();
     }
-
-
-
 }
