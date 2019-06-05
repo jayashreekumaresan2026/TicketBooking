259846b6b20bcfc19005a1fcb847ad7d5d14e805
Jayashree <jayashree.kammu@gmail.com>
2019-06-05 14:30:29 +0530

    Created a new directory and pushed the old ticketbooking files in it

--- src/com/company/ticketbookingsystemoldcode/Passenger.java
+++ src/com/company/ticketbookingsystemoldcode/Passenger.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.company.ticketbookingsystemoldcode;
 
 
 class Passenger {
--- src/com/company/ticketbookingsystemoldcode/Ticket.java
+++ src/com/company/ticketbookingsystemoldcode/Ticket.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.company.ticketbookingsystemoldcode;
 
 import java.util.ArrayList;
 import java.util.List;
