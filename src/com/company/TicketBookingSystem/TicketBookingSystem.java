799e0108b0711d2eacafeb77e6dac316ec03d64f
Jayashree <jayashree.kammu@gmail.com>
2019-06-18 15:00:16 +0530

    displayed ticket with ticket number

--- src/com/company/TicketBookingSystem/TicketBookingSystem.java
+++ src/com/company/TicketBookingSystem/TicketBookingSystem.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
 public class TicketBookingSystem {
     Scanner keyboard = new Scanner(System.in);
 
-
     Passenger getPassengerDetails() {
         System.out.print("enter your name :");
         String name = keyboard.nextLine();
@@ -45,17 +44,20 @@ public class TicketBookingSystem {
             ticketBookingSystem.displayPassengerDetails(passengers, destination, source);
             List<Ticket> tickets = ticketReservationCounter.bookTicket(source, destination, passengers);
             for (int j = 0; j < tickets.size(); j++) {
+                System.out.println("-------Ticket Details--------");
+                System.out.println("Ticket Number :" + (i + 1));
                 System.out.println(tickets.get(j).toString());
             }
         }
     }
+
     void displayPassengerDetails(List<Passenger> passengerList, String source, String destination) {
         String passengers = " ";
-        passengers = "----------Passenger details-------------------"+"\n";
+        passengers = "----------Passenger details-------------------" + "\n";
         passengers += "Source :" + source + "\n";
         passengers += "Destination :" + destination + "\n";
         passengers += "Name\tAge\tGender\n";
-        for(int i=0;i<passengerList.size();i++) {
+        for (int i = 0; i < passengerList.size(); i++) {
             passengers += passengerList.get(i).toString();
         }
         System.out.println(passengers);
