9d591589255e2a0de8680628707abd76d7a79175
Jayashree <jayashree.kammu@gmail.com>
2019-06-19 11:39:24 +0530

    added ticketNumber to the ticket class

--- src/com/company/TicketBookingSystem/Ticket.java
+++ src/com/company/TicketBookingSystem/Ticket.java
@@ -8,26 +8,30 @@ class Ticket {
     private int numberOfPassenger;
     private List<Passenger> passengerList;
     private int totalCost;
+    private int ticketNumber;
 
-    Ticket(String source, String destination, List<Passenger> passengers, int totalCost) {
+    Ticket(String source, String destination, List<Passenger> passengers, int totalCost, int ticketNumber) {
         this.source = source;
         this.destination = destination;
         this.numberOfPassenger = passengers.size();
         this.passengerList = passengers;
         this.totalCost = totalCost;
+        this.ticketNumber = ticketNumber;
     }
 
     @Override
     public String toString() {
-        String ticket =" ";
+        String ticket = " ";
+        ticket = " Ticket Number" + this.ticketNumber;
         ticket += "Source :" + this.source + "\n";
         ticket += "Destination :" + this.destination + "\n";
         ticket += "Number Of passengers :" + this.numberOfPassenger + "\n";
         ticket += "Name\tAge\tGender\n";
-         for(int i=0;i<passengerList.size();i++) {
-                ticket += passengerList.get(i);
-            }
+        for (int i = 0; i < passengerList.size(); i++) {
+            ticket += passengerList.get(i);
+        }
         ticket += "TotalCost : " + this.totalCost;
+        ticket += "---------------------------------";
         return ticket;
     }
 }
